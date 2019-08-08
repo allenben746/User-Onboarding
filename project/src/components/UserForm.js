987613cc173c3bf64fd22ea1eb668da809544659
Ben Allen <BenAllen@Vivians-MacBook-Pro-2.local>
2019-08-08 14:19:55 -0500

    Re-added code that was lost

--- project/src/components/UserForm.js
+++ project/src/components/UserForm.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import axios from 'axios';
+import * as Yup from "yup";
 
 
-const UserForm = () => {
+
+const UserForm = ({ errors, touched, values, status }) => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        if(status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+
     return (
         <div className="userForm">
             <h1>User Sign-Up</h1>
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className='errorMessage'>{errors.name}</p>
+                )}
                 <Field type="text" name="email" placeholder="Email" />
                 <Field type="password" name="password" placeholder="Password" />
                 <label> I agree to the Terms of Service
@@ -16,11 +29,17 @@ const UserForm = () => {
                 </label>
                 <button type="submit">Submit Info</button>
             </Form>
+            {
+                users.map(user => {
+                    return <h3 key={user.id}>{user.username}, {user.id}</h3>
+                })
+            }
         </div>
     )
 }
 
 const FormikUserForm = withFormik({
+    
     //Handles State
     mapPropsToValues({name, email, password}){
         return {
@@ -29,13 +48,27 @@ const FormikUserForm = withFormik({
             password: password || "",
         }
     },
-
-    handleSubmit(values){
+    //Yup Validation
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+        .required('Name is required'),
+        email: Yup.string()
+          .email()
+          .required('Email is required'),
+        password: Yup.string()
+          .min(6)
+          .max(20)
+          .required('A password is required')
+      }),
+    //Post request
+    handleSubmit(values, {setStatus, resetForm}){
         console.log("Form submitted", values);
         axios.post("https://reqres.in/api/users", values)
-        .then(res => console.log(res))
+        .then(res => {
+            setStatus(res.data)
+            resetForm();
+            })
         .catch(err => console.log(err))
     }
 })(UserForm);
-
 export default FormikUserForm;
